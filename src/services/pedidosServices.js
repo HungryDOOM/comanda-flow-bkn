98cccf9d62250b81a15a3e062968953a8909fbf9
HungryDOOM <brianjoelam12_@hotmail.com>
2026-04-23 12:59:17 -0600

    add comanda column to DB

--- src/services/pedidosServices.js
+++ src/services/pedidosServices.js
@@ -1,8 +1,8 @@
 import pool from "../db.js";
 
 async function enviarPedidos(mesa, pedido, precio_total) {
-  const query = "INSERT INTO pedidos (mesa, pedido, precio_total) VALUES ($1, $2, $3) RETURNING *";
-  const values = [mesa, JSON.stringify(pedido), precio_total];
+  const query = "INSERT INTO pedidos (mesa, pedido, precio_total, comanda) VALUES ($1, $2, $3, $4) RETURNING *";
+  const values = [mesa, JSON.stringify(pedido), precio_total, comanda];
   const { rows } = await pool.query(query, values);
   return rows[0];
 }
