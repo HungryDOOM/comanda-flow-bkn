5fb8e61a90bb2addfc0a091b191087d83daf4f98
HungryDOOM <brianjoelam12_@hotmail.com>
2026-04-23 13:38:23 -0600

    fix API

--- src/services/pedidosServices.js
+++ src/services/pedidosServices.js
@@ -1,6 +1,6 @@
 import pool from "../db.js";
 
-async function enviarPedidos(mesa, pedido, precio_total) {
+async function enviarPedidos(mesa, pedido, precio_total, comanda) {
   const query = "INSERT INTO pedidos (mesa, pedido, precio_total, comanda) VALUES ($1, $2, $3, $4) RETURNING *";
   const values = [mesa, JSON.stringify(pedido), precio_total, comanda];
   const { rows } = await pool.query(query, values);
